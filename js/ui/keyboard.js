672a6d3052b858bc3dc131b52c345eea90ab2c79
Carlos Garnacho <carlosg@gnome.org>
2017-12-01 20:17:17 +0100

    keyboard: Ensure to resize keys after layer change
    The keys possibly need resizing after a (new) layer has been set, there's
    however calling places that don't. Instead, fold this._redraw() into
    setActiveLayer().

--- js/ui/keyboard.js
+++ js/ui/keyboard.js
@@ -682,12 +682,10 @@ var Keyboard = new Lang.Class({
 
     _onLevelChanged: function (level) {
         this._setActiveLayer(level);
-        this._redraw();
     },
 
     _onGroupChanged: function () {
         this._setActiveLayer(0);
-        this._redraw();
     },
 
     _onKeyboardGroupsChanged: function(keyboard) {
@@ -704,6 +702,7 @@ var Keyboard = new Lang.Class({
         }
 
         this._current_page = layers[activeLevel];
+        this._redraw();
         this._current_page.show();
     },
 
